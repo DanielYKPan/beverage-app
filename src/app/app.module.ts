865cd00f957922d543a1c40d3bd9cd4afcaf65ed
Yaokun Pan <myron.yk.pan@gmail.com>
2018-08-18 12:54:06 +1000

    Added home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { HomeComponent } from './home/home.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        HomeComponent
     ],
     imports: [
         BrowserModule,
+        AppRoutingModule,
         CoreModule,
     ],
     providers: [],
