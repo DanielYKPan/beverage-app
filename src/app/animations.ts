efdbc0064333888edab85c9538021f67b556ffb0
Yaokun Pan <myron.yk.pan@gmail.com>
2018-08-26 16:05:57 +1000

    Added tileListWrapperAnimations

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -1,11 +1,12 @@
 /**
  * animations
  */
-import { animate, AnimationTriggerMetadata, query, style, transition, trigger } from '@angular/animations';
+import { animate, AnimationTriggerMetadata, group, query, stagger, style, transition, trigger } from '@angular/animations';
 import { detailsFadeOut, listFadeOut } from './product/animations';
 
 export const appAnimations: {
     readonly routerAnimations: AnimationTriggerMetadata;
+    readonly tileListWrapperAnimations: AnimationTriggerMetadata;
 } = {
     routerAnimations: trigger('routerAnimations', [
         transition('product => home', [
@@ -22,5 +23,33 @@ export const appAnimations: {
                 animate('250ms cubic-bezier(.35,0,.25,1)', style({opacity: 1})),
             ], {optional: true})
         ])
-    ])
+    ]),
+
+    tileListWrapperAnimations: trigger('tileListWrapperAnimations', [
+        transition('0 => 1', [
+            group([
+                style({opacity: 0}),
+                animate('100ms', style('*')),
+                query('.tile', [
+                    style({transform: 'translateY(0)'}),
+                    stagger(50, [
+                        animate('300ms', style('*'))
+                    ])
+                ])
+            ]),
+        ]),
+        transition('1 => 0', [
+            style({opacity: 1, bottom: '0', 'pointer-events': 'none'}),
+            query('.tile-list', style({transform: 'translateY({{offsetPlus}}px)'})),
+            group([
+                query('.tile', [
+                    style({transform: 'translateY({{offsetMinus}}px)'}),
+                    stagger(50, [
+                        animate('300ms', style('*'))
+                    ])
+                ])
+            ]),
+            animate('100ms', style({opacity: '*', bottom: '0'})),
+        ])
+    ]),
 };
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,23 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { appAnimations } from '../animations';
 
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    animations: [
+        appAnimations.tileListWrapperAnimations,
+    ]
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
+
+    @ViewChild('listWrapper') listWrapperElm: ElementRef;
 
     public readonly tileWidth = 320;
+    public state = false;
+    public offsetBottom = 0;
+    public listLeft = 0;
 
     public tiles = [
         {
@@ -100,4 +109,28 @@ export class HomeComponent implements OnInit {
     public ngOnInit() {
     }
 
+    public handleMouseEnterOnListWrapper( event: any ) {
+        this.state = true;
+        event.preventDefault();
+    }
+
+    public handleMouseLeaveOnListWrapper( event: any ) {
+        this.state = false;
+        event.preventDefault();
+    }
+
+    public ngAfterViewInit(): void {
+        const listWrapperRect = this.listWrapperElm.nativeElement.getBoundingClientRect();
+        this.offsetBottom = window.innerHeight - listWrapperRect.top - listWrapperRect.height;
+    }
+
+    public handleMouseMoveOnListWrapper( event: MouseEvent ): void {
+        const pageX = event.pageX;
+        const currentProgress = pageX / window.innerWidth;
+        const fromProgress = .1;
+        const toProgress = 1 - fromProgress;
+        const progressDiff = toProgress - fromProgress;
+        const scrollPos = (Math.max(fromProgress, Math.min(toProgress, currentProgress)) - fromProgress) / progressDiff;
+        this.listLeft = scrollPos * (this.tileWidth * this.tiles.length - window.innerWidth) * -1;
+    }
 }
