a022b2e6d48ef10805538ea91bcc8fd7a7779f40
Yaokun Pan <myron.yk.pan@gmail.com>
2018-08-27 20:09:29 +1000

    Fixed layour

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -10,7 +10,7 @@ export const appAnimations: {
 } = {
     routerAnimations: trigger('routerAnimations', [
         transition('product => home', [
-            query(':enter, :leave', style({position: 'absolute', top: 0, left: 0, right: 0}), {optional: true}),
+            query(':enter, :leave', style({position: 'absolute', top: 0, left: 0, width: '100%', height: '100%'}), {optional: true}),
             query(':leave', style({zIndex: 2, opacity: 1}), {optional: true}),
             query(':enter', style({opacity: 0}), {optional: true}),
 
